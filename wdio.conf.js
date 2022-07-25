256d8db3e83ed8f941b443fb39b6d2c86623b8e3
luiz.carvalho <luiz.carvalho@cwi.com.br>
2022-07-25 16:52:44 -0300

    feat: instala e configura allure report

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
 const { join } = require('path')
+const allure = require('allure-commandline')
+const video = require('wdio-video-reporter')
 
 exports.config = {
     hostname: 'localhost',
@@ -20,5 +22,39 @@ exports.config = {
     waitForTimeout: 20000,
     mochaOpts: {
         timeout: 300000
+    },
+    reporters: ['spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+        }],
+        [video, {
+            saveAllVideos: true,       // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+        }]
+    ],
+    onComplete: function () {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function (exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
+    afterStep: async function (step, scenario, { error, duration, passed }, context) {
+        await driver.takeScreenshot();
     }
 }
