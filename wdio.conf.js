a3b82a48b26e41df44035584aa48cc9638e75d74
luiz.carvalho <luiz.carvalho@cwi.com.br>
2022-07-30 18:45:09 -0300

    feat: instala dependências e configura browserstack

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,21 +3,31 @@ const allure = require('allure-commandline')
 const video = require('wdio-video-reporter')
 
 exports.config = {
-    hostname: 'localhost',
-    port: 4723,
-    path: '/wd/hub',
+    // hostname: 'localhost',
+    // port: 4723,
+    // path: '/wd/hub',
+    user: 'luizgustavocouti_NgEf8Y',
+    key: '9qxUSqwtU8CPKsqqDHVC',
+    // services: ['appium'],
+    services: ['browserstack'],
     specs: [
         './test/specs/*.spec.js'
     ],
     maxInstances: 1,
     framework: 'mocha',
     capabilities: [{
-        "platformName": "Android",
-        "platformVersion": "11.0",
-        "deviceName": "sdk_gphone_x86",
-        "automationName": "UiAutomator2",
-        "app": join(process.cwd(), './app/android/wcandroid-7.3.1.apk'),
-        "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity'
+        // "platformName": "Android",
+        // "platformVersion": "11.0",
+        // "deviceName": "sdk_gphone_x86",
+        // "automationName": "UiAutomator2",
+        // "app": join(process.cwd(), './app/android/wcandroid-7.3.1.apk'),
+        // "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity'
+        "app": "bs://835efda53d599e4aa7591899d2d7b3e1a704705d",
+        "device": "Samsung Galaxy Note 20",
+        "os_version": "10.0",
+        "project": "Primeiro projeto em Device Farm",
+        "build": "browserstack-build-1",
+        "name": "teste_login_e_adiciona_produto"
     }],
     waitForTimeout: 20000,
     mochaOpts: {
@@ -30,7 +40,7 @@ exports.config = {
             disableWebdriverScreenshotsReporting: true,
         }],
         [video, {
-            saveAllVideos: true,       // If true, also saves videos for successful test cases
+            saveAllVideos: false,       // If true, also saves videos for successful test cases
             videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
         }]
     ],
